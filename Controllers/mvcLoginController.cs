17f83fe793f4029bafd804e0531076b1195d65b9
GirdharBCT <Girdhar.Sahu@BlueconchTech.com>
2021-09-29 12:09:50 +0530

    Initial commit

---
+++ Controllers/mvcLoginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPhyndLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+
+namespace MVCPhyndLogin.Controllers
+{
+    public class mvcLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autherize(LoginCred loginCred)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44359");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<LoginCred>("authenticate", loginCred);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return RedirectToAction("Index", "mvcLogin"); ;
+
+        }
+    }
+}
---
+++ Models/LoginCred.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPhyndLogin.Models
+{
+    public class LoginCred
+    {
+        [Required(ErrorMessage = "this field is required to continue")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "this field is required to continue")]
+
+        public string Password { get; set; }
+    }
+}
